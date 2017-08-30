26f61cf71f582a5c372e8a229e543c3196cb49ec
xukai <xukai8817wy@163.com>
2017-08-30 16:33:19 +0800

    do my work

--- src/com/edu/HelloWorld.java
+++ src/com/edu/HelloWorld.java
@@ -3,7 +3,8 @@ package com.edu;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("online update!");
+		System.out.println("Hello World!");
+		System.out.println("do my work");
 	}
 	
 }
