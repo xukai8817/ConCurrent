862f3f4b4aa1f86bea2b59589861c16b204a72e1
xukai <xukai8817wy@163.com>
2017-09-14 11:53:24 +0800

    修改之前的commit版本测试

--- src/com/edu/HelloWorld.java
+++ src/com/edu/HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		System.out.println("do my work");
 		System.out.println("merge");
+		
+		System.out.println("32f451150b8ace5a1e34b9681d435cdd57f6c144 修改");
 	}
 	
 }
